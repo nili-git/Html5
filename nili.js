f0c267b02175a717c4faf17fe76e53e2a98f2b05
nili-git <nili7248@gmail.com>
2022-01-24 15:58:19 +0545

    message

---
+++ nili.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+             js program
+        </title>
+    </head>
+    <body>
+  <h2> My first javascript program  </h2>
+  <p> js is client based programming language. Also known as front end web program.</p>
+  <p>  In this case JavaScript changes the value of the src (source) attribute of an image</p>
+  <button onclick="document.getElementById('myImage').src='pic_bulbon.gif'">Turn on the light
+  </button>
+  <img id="myImage" src="pic_bulboff.gif" style="width:100px">
+<button onclick="document.getElementById('myImage').src='pic_bulboff.gif'">Turn off the light </button>
+
+    </body>
+    </html>
